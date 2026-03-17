b63aa3f09a26954d6bcdce7ebd41b67f2a9f2b4e
Quang Tran <quangishere@gmail.com>
2026-03-17 13:14:38 -0700

    test deploy 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Journey Parenting',
+  title: 'Journey to Parenting',
   description: 'Parenting classes for the modern family.',
 };
 
